18240f445b202b2e453cc732eccc6575d9ca04d6
Isaac <isaac@Isaacs-MacBook-Air.local>
2025-08-21 03:37:05 +0300

    fixing gitignoure

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
-const container = document.getElementById("root");
-if (!container)
-    throw new Error("Root container not found");
-const root = ReactDOM.createRoot(container);
-root.render(_jsx(App, {}));
--- dist/videoview.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Videoview = () => {
-    return (_jsx("h1", { children: "Hi" }));
-};
-export default Videoview;
